d85b350fa7defd770b5992c798d1aa069be35634
MohamedElAllouli <alloulialloulimohamed2023@gamil.com>
2024-04-18 19:35:59 -0700

    task

--- console.py
+++ console.py
@@ -114,23 +114,19 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
+        """ Create an object of any class
+        Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with given keys/values and print its id.
+        """
         try:
-            class_name = args.split(" ")[0]
-            if len(class_name) == 0:
-                print("** class name missing **")
-                return
-            if class_name and class_name not in self.valid_classes:
-                print("** class doesn't exist **")
-                return
+            if not args:
+                raise SyntaxError()
+            my_list = args.split(" ")
 
             kwargs = {}
-            commands = arg.split(" ")
-            for i in range(1, len(commands)):
-                key = commands[i].split("=")[0]
-                value = commands[i].split("=")[1]
-                # key, value = tuple(commands[i].split("="))
-                if value.startswith('"'):
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
                     value = value.strip('"').replace("_", " ")
                 else:
                     try:
@@ -140,15 +136,17 @@ class HBNBCommand(cmd.Cmd):
                 kwargs[key] = value
 
             if kwargs == {}:
-                new_instance = eval(class_name)()
+                obj = eval(my_list[0])()
             else:
-                new_instance = eval(class_name)(**kwargs)
-            storage.new(new_instance)
-            print(new_instance.id)
-            storage.save()
-        except ValueError:
-            print(ValueError)
-            return
+                obj = eval(my_list[0])(**kwargs)
+                storage.new(obj)
+            print(obj.id)
+            obj.save()
+
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
 
     def help_create(self):
         """ Help information for the create method """
